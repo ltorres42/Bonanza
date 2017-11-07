f10c9ab8eee3f41696bf8333e2e523557c8342e8
Luis Torres <Luis@Luiss-MacBook-Pro.local>
2017-11-06 16:37:25 -0800

    This is just a test to verify commit capabilities.

---
+++ app/src/main/java/com/example/wendelltan059/bonanza/Videos.java
@@ -0,0 +1,79 @@
+package com.example.wendelltan059.bonanza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+/**
+ * Created by wendell.tan059 on 10/11/17.
+ */
+//test
+
+public class Videos extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+
+
+    public void setting (View view)
+    {
+        Intent intent = new Intent(Videos.this, Settings.class);
+        startActivity(intent);
+
+    }
+
+    public void account_setting (View view)
+    {
+        Intent intent = new Intent(Videos.this, AccountSetting.class);
+        startActivity(intent);
+
+    }
+
+    private static final int RECOVERY_REQUEST = 1;
+    private YouTubePlayerView youTubeView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.videos);
+
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize(Config.YOUTUBE_API_KEY, this);
+    }
+
+    @Override
+    public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
+        if (!wasRestored) {
+            player.cueVideo("NYGCSSP-XSo"); // Plays https://www.youtube.com/watch?v=fhWaJi1Hsfo
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(Provider provider, YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_REQUEST).show();
+        } else {
+            String error = String.format(getString(R.string.player_error), errorReason.toString());
+            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(Config.YOUTUBE_API_KEY, this);
+        }
+    }
+
+    protected Provider getYouTubePlayerProvider() {
+        return youTubeView;
+    }
+}
